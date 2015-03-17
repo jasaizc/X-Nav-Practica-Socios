c40e8673d50dfaf0eaa80a24e74bffcc08b1dc05
jasaizc <ja.saizc@alumnos.urjc.es>
2015-03-18 00:58:10 +0100

    Ampliada Funcionalidad

--- prueba.js
+++ prueba.js
@@ -4,44 +4,77 @@ jQuery(document).ready(function() {
     $(function() {
 		  $( "#tabs" ).tabs();
   	});
-  cambiafrase("SociosTimeline.json")
-  $("#tab1").click(function(){ cambiafrase("SociosTimeline.json")});  
-  $("#tab2").click(function(){ cambiafrase("MiTimeline.json")});       
+  cambiafrase("timeline.json")
+  $("#tab1").click(function(){ cambiafrase("timeline.json")});  
+  $("#tab2").click(function(){ cambiafrase("myline.json")});       
 });
   function cambiafrase(datas)
   {
     words = null;
+   
     $.getJSON( datas, function( data ) {
     words = data;
     }).done(function(){
-      var sentence1 = document.getElementById("tabs-1");
-     var imagen = document.getElementById("imagen1");
-     sentence1.innerHTML = "";
-     for (var i=0 ; i < words.length; i+=1)
+     var tabla = $("#tabs-1").html();
+     if(datas != "update.json")
      {
-        var newwords = "";
-        var nombre =document.createElement("p");
-        var tweet =document.createElement("p");
-        var div =document.createElement("div");
-        var br =document.createElement("br");
-        nombre.innerHTML = words[i].nombre + " " + words[i].fecha;
-        tweet.innerHTML = words[i].tweet;
-        div.setAttribute('style', 'width:400px; height:100px; margin:0');
-        var Img=document.createElement("img");
-        Img.setAttribute('src', words[i].imagen);
-        Img.setAttribute('alt', 'na');
-        Img.setAttribute('height', '60px');
-        Img.setAttribute('width', '60px');
-        Img.setAttribute('style', 'float: left');
-
-        div.setAttribute('id', 'div1');
-        div.appendChild(nombre);
-        div.appendChild(Img);        
-        div.appendChild(tweet); 
-        div.appendChild(br); 
-        sentence1.appendChild(div);
-     }    
+       var menus = $("#tabs-1").accordion( "instance" );
+       if(menus != undefined){
+    			$("#tabs-1").accordion("destroy");
+					tabla = " ";
+          $("#tabs-1").html(tabla);
+				}
+     }
+     var menus = $("#tabs-1").accordion( "instance" );
+     if(menus != undefined){
+        $("#tabs-1").accordion("destroy");
+        tabla2 = tabla;
+        tabla = " ";
+        $("#tabs-1").html(tabla);
+			}
+     
+     var imagen = document.getElementById("imagen1");
+     for (var i=0 ; i < words.Lista.length; i+=1)
+     {
+        var nombre = words.Lista[i].Autor;
+        var fecha = words.Lista[i].Fecha;
+        var titulo = words.Lista[i].Titulo;
+        var imagen = "<img src="+ words.Lista[i].Avatar +" style='width: 60px; height: 60px;'>";
+        var contenido = words.Lista[i].Contenido;
+        tabla += "<h2>"+ imagen + " " + nombre + " - " + titulo + " - " + fecha + "</h2><div><p>"+ contenido +"<br></p></div> ";        
+     }   
+     if(datas == "update.json")
+     {
+        tabla += tabla2;
+        $("#actualizar").html(0);
+     }
+     $("#tabs-1").html(tabla);
+     $("#tabs-1").accordion({active: true});
+     if(datas == "timeline.json")
+     {
+       $.getJSON( "update.json", function( data ) {
+        words1 = data;
+        }).done(function(){
+          $("#actualizar").html(words1.Lista.length);
+          
+        }); 
+     }
+     if(datas == "myline.json")
+     {
+          $("#actualizar").html(0); 
+     }
     }); 
   }
-
+  function Actualizar()
+  {
+    var valor  = document.getElementById('actualizar').innerHTML  
+    if ( valor != 0)
+    {
+      cambiafrase("update.json")
+      
+    }
+    
+    
+    
+  }
 
